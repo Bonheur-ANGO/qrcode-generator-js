d6ca3d173b53e78beeaffe3d9d9eea83490ee622
Bonheur ANGO <ango@et.esiea.fr>
2023-04-06 11:36:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# QR Code Generator
+QR code generator is a tool that allows users to easily create QR codes from text or URLs. To create a QR code we use a JavaScript library : qrcode.js
+
+![demo](/img/Capture-1.png)
+![demo](/img/Capture-2.png)
---
+++ js/script.js
@@ -0,0 +1,70 @@
+const form = document.getElementById('generate-form')
+const qr = document.getElementById('qr-code')
+
+const onGenerateSubmit = (e) =>{
+    e.preventDefault()
+
+    clearQRCode()
+    clearSaveBtn()
+    const url = document.getElementById('url').value;
+    const size = document.getElementById('size').value;
+
+    //console.log(url, size);
+
+    if (url === '') {
+        alert('Please enter a url')
+    } else{
+        showSpinner()
+
+        setTimeout(() =>{
+            hideSpinner()
+
+            generateQRCode(url, size)
+
+            setTimeout(() => {
+                const saveUrl = qr.querySelector('img').src;
+                createSaveBtn(saveUrl)
+            }, 50);
+        }, 1000)
+    }
+}
+
+const generateQRCode = (url, size) =>{
+    const qrcode = new QRCode("qr-code",{
+        text: url,
+        width: size,
+        height: size
+    })
+}
+
+const showSpinner = () =>{
+    document.getElementById('spinner').style.display = 'block'
+}
+
+const hideSpinner = () =>{
+    document.getElementById('spinner').style.display = 'none'
+}
+
+const clearQRCode = () =>{
+    qr.innerHTML = '';
+}
+
+const clearSaveBtn = () =>{
+    qr.innerHTML = '';
+    const saveBtn = document.getElementById('save-link')
+    if (saveBtn) {
+        saveBtn.remove()
+    }
+}
+
+const createSaveBtn = (saveUrl) =>{
+    const link = document.createElement('a')
+    link.id = 'save-link'
+    link.classList = 'bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5'
+    link.href= saveUrl;
+    link.download = 'qrcode'
+    link.innerHTML = 'Save Image'
+    document.getElementById('generated').appendChild(link)
+}
+
+form.addEventListener('submit', onGenerateSubmit)
